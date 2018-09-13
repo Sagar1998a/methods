4a106423affe608dff6568c3423fea912242ada0
Sagar Devkota <30004439@student.toiohomai.ac.nz>
2018-09-13 14:48:51 +1200

    method done

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter anumber");
+            int Number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The input{Number}Changed is :{ChangeSign(Number)}");
+        }
+        public static int ChangeSign(int Number)
+        {
+            return Number *-1;
+        }
+    }
+}
+
+
+
